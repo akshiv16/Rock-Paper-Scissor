c7f4a20082602c34788274b1e1d8465a14376854
Akshiv Bansal <102767077+akshiv16@users.noreply.github.com>
2024-05-13 23:44:23 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,62 @@
+let userscore=0;
+let compscore=0;
+const choices=document.querySelectorAll(".choice");
+
+const msg =document.querySelector("#msgbox");
+const userscorepara=document.querySelector("#your-score");
+const compscorepara=document.querySelector("#comp-score");
+const getcomputerchoice=()=>{
+    const options=["rock","paper","scissor"];
+     const randomindex=Math.floor(Math.random()*3);
+     return options[randomindex];
+};
+const drawgame=()=>{
+    console.log("Game draw");
+    msg.innerText="Game was Draw .Play Again !";
+    msg.style.backgroundColor = "#081b31";
+};
+
+const showWinner=(userwin,userchoice,compchoice)=>{
+    if(userwin){
+        userscore++;
+        userscorepara.innerText=userscore;
+        console.log("You Win");
+        msg.innerText=`You win ! ${userchoice} beats ${compchoice}`;
+        msg.style.backgroundColor = "green";
+    } else
+    { 
+        compscore++;
+        compscorepara.innerText=compscore;
+        console.log("you lose");
+        msg.innerText=`You Lose ! ${compchoice} beats ${userchoice}`;
+        msg.style.backgroundColor = "red";
+    }
+};
+
+const playgame=(userchoice)=>{
+    console.log("user choice=",userchoice);
+    const compchoice=getcomputerchoice();
+  console.log("comp choice =",compchoice);
+  if(userchoice===compchoice)
+    {
+        drawgame();
+    } else 
+    {
+        let userwin=true;
+        if(userchoice==="rock"){
+            userwin=compchoice==="paper" ? false :true;
+        } else if(userchoice==="paper")
+            {
+                userwin=compchoice==="scissor" ?false :true;
+            }else{
+                userwin=compchoice==="rock" ? false :true;
+            }
+            showWinner(userwin,userchoice,compchoice);
+    }
+};
+choices.forEach((choice)=>{
+    choice.addEventListener("click",()=>{
+        const userchoice=choice.getAttribute("id");
+        playgame(userchoice);
+    });
+});
